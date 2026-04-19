381ab86801a27ba2e74ddb3b8699fdc447269e0a
Tolarentsun <Christophersw1011@gmail.com>
2026-04-19 16:34:38 -0400

    Remove unused uuidv4 import
    Removed uuidv4 import from reviews.js

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const { v4: uuidv4 } = require('uuid');
 const db = require('../db/database');
 const { authenticate } = require('../middleware/auth');
 
